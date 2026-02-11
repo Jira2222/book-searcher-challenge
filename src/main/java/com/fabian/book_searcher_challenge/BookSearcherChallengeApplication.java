a8f35c8d32860338ad65a64f7a326e46cc388cb0
Fabian Holguin <ing.fabianholguin@gmail.com>
2026-02-11 14:50:15 -0500

    first commit adding all files

---
+++ src/main/java/com/fabian/book_searcher_challenge/BookSearcherChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.fabian.book_searcher_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSearcherChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSearcherChallengeApplication.class, args);
+	}
+
+}
